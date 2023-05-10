ea8f5eae52a1b2e8bfaf99dc318734a6feda1381
Luciano <luccho277@gmail.com>
2023-05-10 11:30:03 -0300

    Elimine import innecesario

--- csv2jsonind.js
+++ csv2jsonind.js
@@ -1,11 +1,9 @@
-const { log } = require('console');
 const fs = require('fs');
 
 if(process.argv[2]===undefined){
     console.error(`Usage: node csv2json.js <file.csv>`);
     process.exit(1);
 }
-
 const filename = process.argv[2];
 const fileText = fs.readFileSync(filename).toString();
 const allLines = fileText.split('\r\n');
